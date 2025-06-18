7be6bb34e2510a8fcae2c81e7369786c83ff75ab
alegithub01 <alejandrogracia6@gmail.com>
2025-06-18 10:26:08 -0400

    visua

--- app/registro/page.tsx
+++ app/registro/page.tsx
@@ -62,7 +62,6 @@ useEffect(() => {
         console.log("ACCESS TOKEN:", accessToken)
 
         try {
-          console.log(process.env.NEXT_PUBLIC_API_URL);
           const res = await api.post("/auth/register-google", { access_token: accessToken })
           console.log("Respuesta del backend:", res.data)
           alert("Cuenta creada exitosamente")
@@ -75,7 +74,6 @@ useEffect(() => {
       }
     })
   } else {
-    console.log(process.env.NEXT_PUBLIC_API_URL);
     console.error("Google script aún no cargado o window.google no disponible")
   }
 }, [])
